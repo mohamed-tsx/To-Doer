38e6d829a8c872621869dba5c501217f1ad2a308
mohamed-tsx <moamiin79@gmail.com>
2025-04-14 12:38:05 +0300

    fix: Correct company name in Login component and add start script in package.json

--- Client/src/Pages/Login.tsx
+++ Client/src/Pages/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <img src={COMPANY_LOGO_URL} alt="" />
             </div>
-            Halane Market.
+            To Doer.
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
